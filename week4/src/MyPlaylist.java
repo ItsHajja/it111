82164daf0331883e2bc326f0011a1c405d049b84
ItsHajja <hajja.trawally@seattlecolleges.edu>
2025-05-13 21:26:57 -0700

    Add files via upload

---
+++ week4/src/MyPlaylist.java
@@ -0,0 +1,16 @@
+public class MyPlaylist {
+
+    public static void main(String[] args) {
+
+int playlist = 1;
+
+while(playlist <= 10) {
+
+    System.out.println("Playling my songs!");
+    playlist++;
+
+
+    System.out.println("Finished playing my songs!");
+}
+    }
+}
